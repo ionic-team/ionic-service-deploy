a794fc90b86a21669b00642c35fc40e4fcf6ec3b
Joel Weirauch <joel@drifty.com>
2014-10-06 14:55:17 -0500

    Adding some error checking around the plugin function calls to avoid everything crashing if factory calls are made before the plugin has loaded.

--- js/ionicUpdate.js
+++ js/ionicUpdate.js
@@ -54,11 +54,15 @@ angular.module('ionic.services.update', ['ionic.services.common'])
       check: function() {
         var deferred = $q.defer();
 
-        IonicUpdate.check(function(result) {
-          deferred.resolve(result === 'true');
-        }, function(error) {
-          deferred.reject(error);
-        });
+        if (typeof IonicUpdate != "undefined") {
+          IonicUpdate.check(function(result) {
+            deferred.resolve(result === 'true');
+          }, function(error) {
+            deferred.reject(error);
+          });
+        } else {
+          deferred.reject("Plugin not loaded");
+        }
 
         return deferred.promise;
       },
@@ -66,15 +70,19 @@ angular.module('ionic.services.update', ['ionic.services.common'])
       download: function() {
         var deferred = $q.defer();
 
-        IonicUpdate.download(function(result) {
-          if (result !== 'true' && result !== 'false') {
-            deferred.notify(result);
-          } else {
-            deferred.resolve(result === 'true');
-          }
-        }, function(error) {
-          deferred.reject(error);
-        });
+        if (typeof IonicUpdate != "undefined") {
+          IonicUpdate.download(function(result) {
+            if (result !== 'true' && result !== 'false') {
+              deferred.notify(result);
+            } else {
+              deferred.resolve(result === 'true');
+            }
+          }, function(error) {
+            deferred.reject(error);
+          });
+        } else {
+          deferred.reject("Plugin not loaded");
+        }
 
         return deferred.promise;
       },
@@ -82,25 +90,33 @@ angular.module('ionic.services.update', ['ionic.services.common'])
       extract: function() {
         var deferred = $q.defer();
 
-        IonicUpdate.extract(function(result) {
-          if (result !== 'done') {
-            deferred.notify(result);
-          } else {
-            deferred.resolve(result);
-          }
-        }, function(error) {
-          deferred.reject(error);
-        });
+        if (typeof IonicUpdate != "undefined") {
+          IonicUpdate.extract(function(result) {
+            if (result !== 'done') {
+              deferred.notify(result);
+            } else {
+              deferred.resolve(result);
+            }
+          }, function(error) {
+            deferred.reject(error);
+          });
+        } else {
+          deferred.reject("Plugin not loaded");
+        }
 
         return deferred.promise;
       },
 
       load: function() {
-        IonicUpdate.redirect();
+        if (typeof IonicUpdate != "undefined") {
+          IonicUpdate.redirect();
+        }
       },
 
       initialize: function(app_id) {
-        IonicUpdate.initialize(app_id);
+        if (typeof IonicUpdate != "undefined") {
+          IonicUpdate.initialize(app_id);
+        }
       }
     }
 }])
